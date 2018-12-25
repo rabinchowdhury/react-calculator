be93a5ba80d125bbaee2eff0d26e2fb8ba859c34
rabinchowdhury <rabinc.dev@gmail.com>
2018-12-24 20:18:18 -0500

    Save before ejecting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import "./app.css";
+import Button from '../components/Button.jsx';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="app">
+        <div className="calc-wrapper">
+          <div className="row">
+            <Button>7</Button>
+            <Button>8</Button>
+            <Button>9</Button>
+            <Button>/</Button>
+          </div>
+        </div>
       </div>
     );
   }
